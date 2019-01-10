cf340ea0053a1fddcde47763c7fe1fa183b92c6e
abalasu1 <abalasu1@in.ibm.com>
2019-01-10 23:49:45 +0530

    fortune teller - discovery service v0.1

---
+++ ft-discovery-service/src/main/java/com/fortuneteller/discoveryservice/FtDiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.fortuneteller.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FtDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtDiscoveryServiceApplication.class, args);
+	}
+
+}
+
